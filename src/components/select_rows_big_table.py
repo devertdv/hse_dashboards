4c96cd131bc6e2ce80e3e429dd8e7319a622d8e0
Darko Vrtsel <devert.dv@gmail.com>
2023-03-06 07:35:28 +0300

    Fixed: display selected rows of the big table in the beginning

--- src/components/select_rows_big_table.py
+++ src/components/select_rows_big_table.py
@@ -3,7 +3,10 @@ def select_rows_big_table(df, click_data):
     select_rows = []
     if click_data is not None and "entry" in click_data["points"][0] and click_data["points"][0]["entry"] != click_data["points"][0]["label"]:
         keyword = click_data["points"][0]["label"]
-        select_rows = df.index[df["Author Keywords"].str.contains(keyword, regex=False)].tolist()
+        for idx, row in df.iterrows():
+            cell = row["Author Keywords"].split("; ")
+            if keyword in cell:
+                select_rows.append(idx)
     else:
         select_rows = [i for i in range(df.shape[0])]
 
